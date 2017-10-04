ea5f024c3be605af7f7b65cea8570867a5a59f81
Jonathan Prieto-Cubides <jprieto9@eafit.edu.co>
2017-10-04 11:22:03 -0500

    [ ref.bib ] added a new one ref.

--- paper/fix-references.sh
+++ paper/fix-references.sh
@@ -2,5 +2,5 @@
 
 sed -i -r "/(url|doi)/ s/\{.?(.)\}/\1/g" ref-mendeley.bib
 sed -i -r "s/journaltitle/journal     /g" ref-mendeley.bib
+sed -i -r "s/@inbook/@incollection/g" ref-mendeley.bib
 sed -i -r "/file/d" ref-mendeley.bib
-
